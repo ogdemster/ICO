03ee7bb3a82a761a5ffc4d21b2d9fa7a5d6ec8c0
Özgür Öğdem <ogdemster@gmail.com>
2022-09-22 12:01:30 +0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ICO
+
+Crypto Devs Token Contract Address: 0xb75FF0FB2dd7f009Cc8C1953C8Fb6E03013a97A4
---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x20eb0077561B57E3359F78a61F072cB59cD4864A";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
